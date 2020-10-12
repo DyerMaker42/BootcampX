61bfd6d9f997e2b211ea890499c5b8a1c5a5725c
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-10-12 22:07:11 +0000

    inital commit

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,32 @@
+-- CREATE TABLE cohorts (id primary, 
+-- name VARCHAR(30),
+-- start_date DATE,
+-- end_date DATE,);
+
+-- CREATE TABLE students (id primary,
+-- name VARCHAR(30),
+-- email VARCHAR(30),
+-- phone integer,
+-- github VARCHAR(30),
+-- start_date DATE,
+-- end_date DATE,
+-- cohort_id VARCHAR(30));
+
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
+
